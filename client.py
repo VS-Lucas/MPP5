6430b62eca1b0b68eaec11df2d6ce96e63182b8a
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2022-05-20 10:14:49 -0300

    ultimo

--- client.py
+++ client.py
@@ -15,7 +15,8 @@ class Client:
     def buildSocketUDP(self) -> None:
         # Criando o canal de comunicação TCP entre client -> servidor
         self.sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sckt.connect(self.con)
+        self.sckt.bind(('192.168.0.105', 55555))
+        # self.sckt.connect(self.con)
 
     def sendfile(self) -> None:
         pathsize = os.path.getsize(self.path)
@@ -28,39 +29,44 @@ class Client:
             try:
                 startTime = time.time()
                 while pathsize > 0:
+                    # if 220322835 - pathsize >= 104857600:
+                    #     break
                     self.cont += 1
                     print(f'Pacote: {self.cont} | {pathsize} bytes restantes para o upload')
    
                     self.sckt.sendto(byte_data, self.con)
                     byte_data = file.read(self.BUFFER)
                     pathsize -= self.BUFFER
+        
             except:
                 print('ERROR')
-                
+        
         self.taxaPerdaUp  = pathsize / 56427430
         self.uploadTime   = time.time() - startTime
         self.receivedDataUp   = 56427430 - pathsize
-        self.taxaVazaoUP      = (self.receivedData)/(self.downloadTime)
+        self.taxaVazaoUP      = (self.receivedDataUp)/(self.uploadTime)
         print('Arquivo enviado com sucesso!!')
+        self.sckt.sendto(bytes(f'{byte_data} enviado', 'utf-8'), self.con)
     
     def recvfile(self) -> None:
         pathsize = os.path.getsize(self.path)
         self.sckt.recv(1024)
         with open('server.pdf', 'wb') as file:
-            byte_data = self.sckt.recv(self.BUFFER)
+            byte_data = self.sckt.recvfrom(self.BUFFER)[0]
             pathsize -= self.BUFFER
             startTime = time.time()
             while pathsize > 0:
+                
                 self.cont += 1
                 print(f'Pacote: {self.cont} | {pathsize} bytes restantes para o dowload')
                 file.write(byte_data)
-                byte_data = self.sckt.recv(self.BUFFER)
+                byte_data = self.sckt.recvfrom(self.BUFFER)[0]
                 pathsize -= self.BUFFER
-                
+            
         self.downloadTime     = time.time() - startTime
         self.taxaPerdaDown    = pathsize / 56427430
         self.receivedDataDown   = 56427430 - pathsize
-        self.taxaVazaoDown      = (self.receivedData)/(self.uploadTime)
+        self.taxaVazaoDown      = (self.receivedDataDown)/(self.downloadTime)
         print('Arquivo recebido!!')
         file.close()
 
@@ -76,27 +82,27 @@ class Client:
         
         data = self.scktTCP.recv(1024).decode()
         TaxaPerdaDown, VazaoDown, TempoDown = data.split()
-        print(f'Taxa de perda do upload Cliente -> Servidor: {self.taxaPerdaUp*100}')
+        print(f'Taxa de perda do upload Cliente -> Servidor: {self.taxaPerdaUp*100}%')
         print(f'Vazão do upload Cliente -> Servidor: {self.taxaVazaoUP}')
         print(f'Tempo de upload Cliente -> Servidor: {self.uploadTime}\n')
      
-        print(f'Taxa de perda de download do Servidor: {TaxaPerdaDown*100}')
+        print(f'Taxa de perda de download do Servidor: {TaxaPerdaDown*100}%')
         print(f'Vazão de download do Servidor: {VazaoDown}')
-        print(f'Tempo de download do Servidor: {TempoDown}')
+        print(f'Tempo de download do Servidor: {TempoDown}\n')
         
         data = self.scktTCP.recv(1024).decode()
         TaxaPerdaUp, VazaoUp, TempoUp = data.split()
-        print(f'Taxa de perda do upload Servidor -> Client: {TaxaPerdaUp*100}')
+        print(f'Taxa de perda do upload Servidor -> Client: {TaxaPerdaUp*100}%')
         print(f'Vazão do upload Servidor -> Cliente: {VazaoUp}')
         print(f'Tempo de upload Servidor -> Cliente: {TempoUp}\n')
      
-        print(f'Taxa de perda de download do Cliente: {self.taxaPerdaDown*100}')
+        print(f'Taxa de perda de download do Cliente: {self.taxaPerdaDown*100}%')
         print(f'Vazão de download do Cliente: {self.taxaVazaoDown}')
-        print(f'Tempo de download do Cliente: {self.downloadTime}')
+        print(f'Tempo de download do Cliente: {self.downloadTime}\n')
         pass
     
     def status(self):    
         pass
 
-client = Client('179.152.253.19', 55555, "path-54MB.txt", 100)
+client = Client('179.152.253.19', 55555, "path-54MB.pdf", 1024)
 
